a8858cdae8d3079249d4cc120f7177273cf66e38
Boboaca Costel <bobyca2003@yahoo.com>
2026-07-30 15:52:15 +0200

    Refactor push_led_on.asm for consistent formatting

--- arduino-forum/call/push_led_on.asm
+++ arduino-forum/call/push_led_on.asm
@@ -4,10 +4,10 @@
 .org 0x60
 init:
 	ldi r16,0
-        push r16
-        ldi r16,0x65
-        push r16
-        rjmp 0x66
+    push r16
+    ldi r16,0x65
+    push r16
+    rjmp 0x66
 loop:
     rjmp loop  
 led_on:
